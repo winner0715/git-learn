9266ff7d6f620eb998469e868354d9b2d043a91e
gaohongchao002 <gaohongchao002@ke.com>
2022-03-03 11:10:37 +0800

    merge to one

--- Test.java
+++ Test.java
@@ -3,6 +3,11 @@
  * @date 2022/3/3
  */
 public class Test {
+	public static void main(String[] args) {
+		System.out.println("dev commit 2");
+		System.out.println("dev commit 3");
+
+	}
 }
 
 class Test1{
